7da0ba39fac2d3bb304f1cc5177cf197c4624b26
Kasai Kou <80010105+kasaikou@users.noreply.github.com>
2023-08-27 18:02:54 +0900

    fix localization

--- background.js
+++ background.js
@@ -1,19 +1,28 @@
+const tweetWithUrl = chrome.i18n.getMessage("tweetWithUrl")
+const tweetWithUrlAndText = chrome.i18n.getMessage("tweetWithUrlAndText")
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.contextMenus.create({
     "id": "quoteTweetWithText",
-    "title": tweetTextWithUrlAndText,
+    "title": tweetWithUrlAndText,
     "contexts": ["selection"]
   });
   chrome.contextMenus.create({
     "id": "quoteTweet",
-    "title": tweetTextWithUrl,
+    "title": tweetWithUrl,
     "contexts": ["page"]
   });
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  if (info.menuItemId === "quoteTweetWithText" || info.menuItemId === "quoteTweet") {
-    const tweetText = info.selectionText == undefined ? `${tab.title} ${url}` : `> ${info.selectionText}\n${tab.title} ${tab.url}`;
+  if (info.menuItemId === "quoteTweetWithText") {
+    const tweetText = `> ${info.selectionText}\n${tab.title} ${tab.url}`;
+    chrome.tabs.create({
+      url: `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`
+    });
+
+  } else if (info.menuItemId === "quoteTweet") {
+    const tweetText = `${tab.title} ${url}`;
     chrome.tabs.create({
       url: `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`
     });
