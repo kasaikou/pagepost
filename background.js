cc2d953148f264f794d01a31a80708f6d3701092
Kasai Kou <80010105+kasaikou@users.noreply.github.com>
2023-08-27 17:55:13 +0900

    localize and add add `tweet with url`

--- background.js
+++ background.js
@@ -1,13 +1,18 @@
 chrome.runtime.onInstalled.addListener(function() {
   chrome.contextMenus.create({
-    "id": "quoteTweet",
-    "title": "この文章を引用してツイート",
+    "id": "quoteTweetWithText",
+    "title": tweetTextWithUrlAndText,
     "contexts": ["selection"]
+  });
+  chrome.contextMenus.create({
+    "id": "quoteTweet",
+    "title": tweetTextWithUrl,
+    "contexts": ["page"]
   });
 });
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  if (info.menuItemId === "quoteTweet") {
+  if (info.menuItemId === "quoteTweetWithText" || info.menuItemId === "quoteTweet") {
     const tweetText = info.selectionText == undefined ? `${tab.title} ${url}` : `> ${info.selectionText}\n${tab.title} ${tab.url}`;
     chrome.tabs.create({
       url: `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`
